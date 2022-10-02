50bc37e893337ecb677b80d57be0edccbf9dbff6
Jennifer-Roatt <114031964+Jennifer-Roatt@users.noreply.github.com>
2022-10-01 22:06:34 -0300

    Update CalculadoraC.c

--- CalculadoraC.c
+++ CalculadoraC.c
@@ -16,44 +16,53 @@ int main() {
 
   scanf("%i", &operacao); // entrada de dados da opcao digitada no menu 
   printf("Sua opção digitada foi %i\n",operacao);
-  printf("Agora digite o primeiro número para realizar o calculo:");
-  scanf("%f", &a); // primeiro número 
-  printf("Seu primeiro número digitado foi: %f \n", a);
-  
-if(operacao != 5) {  // para poder usar a raiz tive que criar um if, pq como na raiz quadrada precisa só de um número para realizar o calculo. 
-  printf("Agora digite o segundo número para realizar o calculo:");
-  scanf("%f", &b);
-  printf("Seu segundo número digitado foi: %f \n", b); // qualquer entrada diferente da opcao 5 cai aqui
-    }
-    
-  switch (operacao) {   // usei a estrutura swith pq já tinha familiaridade, pois tudo fica mais organizado.
-  case 1:
-    resultado = a + b;
-    printf("O resultado da adição de %f e %f é: %f", a, b, resultado); // a e b = resultado do calcuclo 
-    break;
-  case 2:
-    resultado = a - b;
-     printf("O resultado da subtração de %f e %f é: %f", a, b, resultado);
-    break;
-  case 3:
-    resultado = a * b;
-     printf("O resultado da multiplicação de %f e %f é: %f", a, b, resultado);
-    break;
-  case 4:
-    resultado = a / b;
-     printf("O resultado da divisão de %f e %f é: %f", a, b, resultado);
-    break;
-    case 5: 
-      resultado = sqrt(a); // tive que pesquisar como era a raiz quadrada, pois não sabia como deveria ser feito.
-      printf("O resultado da raiz quadrada de %f  é %f:",a, resultado);
-    break;
-    case 6:
-    resultado = pow (a,b); // tive que pesquisar como era a potencia, pois não sabia como deveria ser feito.
-      printf("O resultado da pontencia de %f e %f é: %f", a, b, resultado);
-    break;
-  default:
+
+  if(!(operacao >= 1 && operacao <= 6)){
     printf("\n Opção inválida");
   }
+  else{
+    printf("Agora digite o primeiro número para realizar o calculo:");
+    scanf("%f", &a); // primeiro número 
+    printf("Seu primeiro número digitado foi: %f \n", a);
+    
+  if(operacao != 5) {  // para poder usar a raiz tive que criar um if, pq como na raiz quadrada precisa só de um número para realizar o calculo. 
+    printf("Agora digite o segundo número para realizar o calculo:");
+    scanf("%f", &b);
+    printf("Seu segundo número digitado foi: %f \n", b); // qualquer entrada diferente da opcao 5 cai aqui
+      }
+      
+    switch (operacao) {   // usei a estrutura swith pq já tinha familiaridade, pois tudo fica mais organizado.
+    case 1:
+      resultado = a + b;
+      printf("O resultado da adição de %f e %f é: %f", a, b, resultado); // a e b = resultado do calcuclo 
+      break;
+    case 2:
+      resultado = a - b;
+       printf("O resultado da subtração de %f e %f é: %f", a, b, resultado);
+      break;
+    case 3:
+      resultado = a * b;
+       printf("O resultado da multiplicação de %f e %f é: %f", a, b, resultado);
+      break;
+    case 4:
+       if(b == 0){
+         printf("Não é possível dividir por zero.");
+       }
+      else{
+        resultado = a / b;
+        printf("O resultado da divisão de %f e %f é: %f", a, b, resultado);
+      }  
+      break;
+      case 5: 
+        resultado = sqrt(a); // tive que pesquisar como era a raiz quadrada, pois não sabia como deveria ser feito.
+        printf("O resultado da raiz quadrada de %f  é %f:",a, resultado);
+      break;
+      case 6:
+      resultado = pow (a,b); // tive que pesquisar como era a potencia, pois não sabia como deveria ser feito.
+        printf("O resultado da pontencia de %f e %f é: %f", a, b, resultado);
+      break;
+    }
+  }  
  
   return 0;
 }
